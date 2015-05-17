091d3d9f78906ddbbe9a0d468ddf2dc8892dee83
Ian Webster <ianw_git@ianww.com>
2015-05-16 18:10:26 -0700

    initial commit

---
+++ catfacts_app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+var stripe = require('stripe')(
+  require('./config.js').STRIPE_SK
+);
+
+app.get('/', function(req, res) {
+  serveFile('index.html', res);
+});
+
+app.get('/pay', function (req, res) {
+  stripe.charges.create({
+    amount: req.query.amount,
+    currency: 'usd',
+    source: req.query.tok,
+    description: 'Charge for cat facts - ' + req.query.email,
+  }, function(err, charge) {
+    if (err) {
+      console.log(err, charge);
+      res.send('error');
+      return;
+    }
+    res.send('ok');
+  });
+});
+
+function serveFile(path, res) {
+  fs.readFile(path, function (err, data){
+    res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+    res.write(data);
+    res.end();
+  });
+}
+
+var server = app.listen(8111, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
